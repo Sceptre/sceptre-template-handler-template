38cb0a994bdce564bc794835dbccb2da45563cdc
Jon Falkenstein <jfalkenstein@trueaccord.com>
2022-03-05 15:38:21 -0600

    undoing changelog and version since this is a template

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-__version__ = "0.1.1"
+__version__ = "0.1.0"
 
 # More information on setting values:
 # https://github.com/Sceptre/project/wiki/sceptre-template-handler-template
